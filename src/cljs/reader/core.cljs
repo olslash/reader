5fd810af81a074579cd7617ceb1ea69169837fe6
Mitch R <olordhux+github@gmail.com>
2015-07-15 23:45:39 -0700

    project base

---
+++ src/cljs/reader/core.cljs
@@ -0,0 +1,16 @@
+(ns reader.core
+    (:require [reagent.core :as reagent]
+              [re-frame.core :as re-frame]
+              [reader.handlers]
+              [reader.subs]
+              [reader.routes :as routes]
+              [reader.views :as views]))
+
+(defn mount-root []
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn ^:export init [] 
+  (routes/app-routes)
+  (re-frame/dispatch-sync [:initialize-db])
+  (mount-root))
---
+++ src/cljs/reader/db.cljs
@@ -0,0 +1,4 @@
+(ns reader.db)
+
+(def default-db
+  {:name "re-frame"})
